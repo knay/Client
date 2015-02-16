e2e7961ff20ae51465773d824e1203a975aaff97
david ansillon <david.ansillon@gmail.com>
2015-02-16 16:37:04 +0100

    commit lundi

--- src/Imerir/NoyauBundle/Controller/DefaultController.php
+++ src/Imerir/NoyauBundle/Controller/DefaultController.php
@@ -8,12 +8,12 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-    	ini_set("soap.wsdl_cache_enabled", 0);
-    	//todo mettre l'url dans un twig de ressources
-    	$client = new \SoapClient('http://192.168.1.60/serveur/web/app_dev.php?wsdl');
-    	$result = $client->__soapCall('hello', array('name'=>'DUCON'));
+//     	ini_set("soap.wsdl_cache_enabled", 0);
+//     	//todo mettre l'url dans un twig de ressources
+//     	$client = new \SoapClient('http://192.168.1.60/serveur/web/app_dev.php?wsdl');
+//     	$result = $client->__soapCall('hello', array('name'=>'DUCON'));
     	
-        return $this->render('ImerirNoyauBundle:Default:index.html.twig', array('soap_return'=>$result));
+        return $this->render('ImerirNoyauBundle:Default:index.html.twig');
     }
     
     public function authentificationAction()
@@ -26,8 +26,11 @@ class DefaultController extends Controller
     	return $this->render('ImerirNoyauBundle:Default:test.html.twig');
     }
     
-    public function baseAction()
+    public function getindexAction()
     {
-    	return $this->render('ImerirNoyauBundle:Default:index.html.twig');
+    	$query = $this->getRequest();
+    	$nom = $query->get('utilisateur');
+    	$prenom = $query->get('mot_de_passe');
+    	return $this->render('ImerirNoyauBundle:Default:index.html.twig',array('utilisateur' => $nom,'mot_de_passe' => $prenom));
     }
 }

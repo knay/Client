b3c17781a73b546682609cb804e1cd9d437bf3d9
Etienne Dijon <etiennedijon@MBP-de-Etienne.imerir.lan>
2015-02-17 13:47:00 +0100

    ajout de creation produit

--- src/Imerir/ProduitBundle/Controller/DefaultController.php
+++ src/Imerir/ProduitBundle/Controller/DefaultController.php
@@ -16,4 +16,19 @@ class DefaultController extends Controller
         echo $result;
         return $this->render('ImerirProduitBundle::ajoutAttribut.html.twig');
     }
+
+    /**
+     * fonction pour créer un produit
+     */
+    public function creerProduitAction(){
+        //TODO ajouter un service qui gere le client soap
+        ini_set("soap.wsdl_cache_enabled", 0);
+        $client = new \SoapClient('http://localhost/serveur/web/app_dev.php/soap');
+        $sessToken = $this->container->get('request')->getSession()->get('token');
+        $client->__setCookie('PHPSESSID',$sessToken);
+        /*
+        $result = $client->__soapCall('newproduct');
+        */
+        return $this->render('ImerirProduitBundle::creerProduit.html.twig', array('utilisateur' => 'jojo','groupe' => 'toto'));
+    }
 }

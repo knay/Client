87083280aae51e6c775ca3e95e38406a635b23c0
Knay <payen.john12@gmail.com>
2015-02-17 09:35:44 +0100

    qsdsqd

--- src/Imerir/ProduitBundle/Controller/AttributController.php
+++ src/Imerir/ProduitBundle/Controller/AttributController.php
@@ -11,13 +11,13 @@ class AttributController extends Controller
     {
     	//ini_set("soap.wsdl_cache_enabled", 0);
     	//todo mettre l'url dans un twig de ressources
+    	
     	$client = new \SoapClient('http://localhost/serveur/web/app_dev.php/soap');
     	//$client->__setCookie('PHPSESSID', $this->getRequest()->cookies->get('PHPSESSID'));
     	$result = $client->__soapCall('login', array('username'=>'alba', 'passwd'=>'alba'));
-//    	$token = new UsernamePasswordToken($u->getUsername(), $u->getPassword(), 'main', $u->getRoles());
-    	
-    	print_r($result);
-        return $this->render('ImerirProduitBundle::ajoutAttribut.html.twig');
+		//$token = new UsernamePasswordToken($u->getUsername(), $u->getPassword(), 'main', $u->getRoles());
+    	//print_r($result);
+        return $this->render('ImerirProduitBundle::ajoutAttribut.html.twig', array('utilisateur' => 'jojo','groupe' => 'toto'));
     }
     
     public function saveAttributAction()

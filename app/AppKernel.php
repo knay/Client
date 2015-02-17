3f80b595d427c8dea413b8f84d29ec2da8860373
Knay <payen.john12@gmail.com>
2015-02-17 13:20:29 +0100

    khgk

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Imerir\NoyauBundle\ImerirNoyauBundle(),
             new Imerir\ProduitBundle\ImerirProduitBundle(),
+        	new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {

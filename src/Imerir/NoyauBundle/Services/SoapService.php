1c2ab356d0ba439dd2f6fc24232a7d3dd6d90240
Knay <payen.john12@gmail.com>
2015-02-17 15:02:09 +0100

    Commentaire

--- src/Imerir/NoyauBundle/Services/SoapService.php
+++ src/Imerir/NoyauBundle/Services/SoapService.php
@@ -3,11 +3,25 @@ namespace Imerir\NoyauBundle\Services;
 
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
+/**
+ * Service permettant de faire des appels SOAP.
+ * Ce service s'occupe de la gestion du token de connexion au serveur SOAP.
+ * Pour se connecter au serveur soap faire appel à la fonction login().
+ */
 class SoapService
 {
-	private $client;
-	private $container;
+	private $client; // Le client SOAP
+	private $container; // Le container symfony pour récupérer l'url du serveur dans la config
 	
+	
+	/**
+	 * Constructeur de la classe.
+	 * Initialise la connexion avec le serveur SOAP.
+	 * 
+	 * TODO gérer les entrée utilisateur
+	 * TODO gérer cas d'erreur
+	 * @param unknown $container Container symfony pour récupérer l'url du serveur.
+	 */
     public function __construct($container) {
     	$this->container = $container;
     	
@@ -15,6 +29,13 @@ class SoapService
     	$this->client = new \SoapClient($url);
     }
     
+    /**
+     * Permet de passer un appel SOAP.
+     * 
+     * @param unknown $methode Le nom de la fonction a appeler.
+     * @param unknown $args Les arguments à passer à SOAP (array)
+     * @return mixed La reponse SOAP.
+     */
     public function call($methode, $args) {
     	$session = $this->container->get('request')->getSession();
     	$token = $session->get('token');
@@ -25,22 +46,30 @@ class SoapService
     	return $this->client->__soapCall($methode, $args);
     }
     
+    /**
+     * Permet de se logger sur le serveur distant.
+     * 
+     * TODO Vérifier arguments
+     * @param unknown $usr Le nom d'utilisateur.
+     * @param unknown $passwd Le mot de passe.
+     */
     public function login($usr, $passwd) {
-    	$this->client->__setCookie('PHPSESSID', uniqid());
+    	$this->client->__setCookie('PHPSESSID', uniqid()); // On génére un numero pour la session
     	 
     	try {
+    		// On demande à se logger
     		$result = $this->client->__soapCall('login', array('username' => $usr, 'passwd' => $passwd));
     		$result_json = json_decode($result, true);
     	} catch (Exception $e) {
-    		
+    		//TODO gérer ça 
     	}
     	
     	$result_json = json_decode($result, true);
     	$token = $result_json['token'];
-    	$this->container->get('request')->getSession()->set('token', $token);
+    	$this->container->get('request')->getSession()->set('token', $token); // On met le token dans la session
     	
     	$role = array();
-    	$role[0] = $result_json['role'];
+    	$role[0] = $result_json['role']; // Récupération du role pour créer la session symfony
     	$token = new UsernamePasswordToken($usr, $passwd,'main', $role);
     	$context = $this->container->get('security.context');
     	$context->setToken($token);

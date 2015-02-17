6dd1213386f3199f48bd2e1d11d241dc297ada8a
Knay <payen.john12@gmail.com>
2015-02-17 14:48:08 +0100

    dqsd

--- src/Imerir/NoyauBundle/Controller/DefaultController.php
+++ src/Imerir/NoyauBundle/Controller/DefaultController.php
@@ -29,24 +29,8 @@ class DefaultController extends Controller
     	$nom = $query->get('utilisateur');
     	$mot_de_passe = $query->get('mot_de_passe');
     	
-    	/*$client = new \SoapClient('http://localhost/serveur/web/app_dev.php/soap');
-    	$client->__setCookie('PHPSESSID', 'totolitoto');
-    	
-	    try {
-	    	$result = $client->__soapCall('login', array('username'=>$nom, 'passwd'=>$mot_de_passe));
-			$session_token = uniqid();
-			$this->container->get('request')->getSession()->set('token',$session_token);
-			$client->__setCookie('PHPSESSID',$session_token);
-		} catch (Exception $e) {
-			echo '<script type="text/javascript">window.alert("'.$e.'");</script>';
-		}
-		$result_json = json_decode($result,true);
-		$role[0] = $result_json['role'];
-		
-		$token = new UsernamePasswordToken($nom, $mot_de_passe,'main', $role);
-		$context = $this->container->get('security.context');
-		$context->setToken($token);*/
-    	
+    	// Récupération du service soap et demande de login
+    	// TODO gérer les soapfault
     	$soap = $this->get('noyau_soap');
 		$soap->login($nom, $mot_de_passe);
 		

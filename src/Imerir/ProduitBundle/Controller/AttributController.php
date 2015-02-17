08a5be988ab451d04c1612035f8079e5b738f69b
Etienne Dijon <etiennedijon@MacBook-Pro-de-Etienne.local>
2015-02-17 09:20:26 +0100

    rectification erreur fusion

--- src/Imerir/ProduitBundle/Controller/AttributController.php
+++ src/Imerir/ProduitBundle/Controller/AttributController.php
@@ -12,12 +12,9 @@ class AttributController extends Controller
     	//ini_set("soap.wsdl_cache_enabled", 0);
     	//todo mettre l'url dans un twig de ressources
     	$client = new \SoapClient('http://localhost/serveur/web/app_dev.php/soap');
-<<<<<<< HEAD
         //permet de gérer le token par les cookies
         $client->__setCookie('PHPSESSID',uniqid());
-=======
     	//$client->__setCookie('PHPSESSID', $this->getRequest()->cookies->get('PHPSESSID'));
->>>>>>> f2fad40849268f5dc92950a0fdb0f4025008393f
     	$result = $client->__soapCall('login', array('username'=>'alba', 'passwd'=>'alba'));
 //    	$token = new UsernamePasswordToken($u->getUsername(), $u->getPassword(), 'main', $u->getRoles());
     	

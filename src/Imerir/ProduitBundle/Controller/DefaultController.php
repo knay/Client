711e66ca21b72bbfc3f7bee2c6aea531bd74beef
Etienne Dijon <etiennedijon@MBP-de-Etienne.imerir.lan>
2015-02-18 16:16:32 +0100

    client ajoutProduit ok

--- src/Imerir/ProduitBundle/Controller/DefaultController.php
+++ src/Imerir/ProduitBundle/Controller/DefaultController.php
@@ -17,18 +17,35 @@ class DefaultController extends Controller
         return $this->render('ImerirProduitBundle::ajoutAttribut.html.twig');
     }
 
-    /**
-     * fonction pour créer un produit
-     */
-    public function creerProduitAction(){
-        //TODO ajouter un service qui gere le client soap
-        ini_set("soap.wsdl_cache_enabled", 0);
-        $client = new \SoapClient('http://localhost/serveur/web/app_dev.php/soap');
-        $sessToken = $this->container->get('request')->getSession()->get('token');
-        $client->__setCookie('PHPSESSID',$sessToken);
-        /*
-        $result = $client->__soapCall('newproduct');
-        */
-        return $this->render('ImerirProduitBundle::creerProduit.html.twig', array('utilisateur' => 'jojo','groupe' => 'toto'));
+    public function ajoutProduitAction()
+    {
+        $soap = $this->get('noyau_soap');
+        //on récupère toutes les lignes produits
+        $return_lp = $soap->call('getLigneProduit', array('count' => 0,'offset' => 0, 'nom' => ''));
+        $lignesProduitsretour = json_decode($return_lp);
+
+        //on appelle la fonction ajoutLigneProduit du serveur soap qui prend en parametre le nom de la ligne produit
+        return $this->render('ImerirProduitBundle::creerProduit.html.twig', array('ligne_produit' => $lignesProduitsretour));
+    }
+
+    public function execAjoutProduitAction()
+    {
+
+        $query = $this->get('request');
+        $nom = $query->request->get('nomProduit');
+        $ligneProduit = $query->request->get('nomLigneProduit');
+
+        $soap = $this->get('noyau_soap');
+        //on appelle la fonction ajoutLigneProduit du serveur soap qui prend en parametre le nom de la ligne produit
+        $return = $soap->call('ajoutProduit',array('nom' => $nom,'ligneProduit' => $ligneProduit));
+
+        //on récupère toutes les lignes produits
+        $return_lp = $soap->call('getLigneProduit', array('count' => 0,'offset' => 0, 'nom' => ''));
+        $lignesProduitsretour = json_decode($return_lp);
+
+        //on appelle la fonction ajoutLigneProduit du serveur soap qui prend en parametre le nom de la ligne produit
+        return $this->render('ImerirProduitBundle::creerProduit.html.twig', array('ligne_produit' => $lignesProduitsretour));
+
+
     }
 }

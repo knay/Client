1d54a03720c7ecc466b05fed00d52ca4fb50cd87
david ansillon <david.ansillon@gmail.com>
2015-02-22 21:50:11 +0100

    rrr

--- src/Imerir/StockBundle/Controller/InventaireController.php
+++ src/Imerir/StockBundle/Controller/InventaireController.php
@@ -14,13 +14,15 @@ class InventaireController extends Controller
     	$return_produits = $soap->call('getProduit', array('count'=>0, 'offset'=>0, 'nom'=>'', 'ligneproduit'=>''));
     	$produitsRetour = json_decode($return_produits);
     	
-    	
-    	
     	$attributs = $soap->call('getAttributFromNomProduit', array('nom'=>$produitsRetour[0]->p));
 
+    	$return_menu = $soap->call('getMenu', array());
+    	$menu_sous_menu = json_decode($return_menu);
+    	
         return $this->render('ImerirStockBundle::inventaire.html.twig', 
         	   array('produit'=>$produitsRetour,
-        			 'attributs'=>json_decode($attributs)
+        			 'attributs'=>json_decode($attributs),
+        	   		'result_menu' => $menu_sous_menu
                ));
     }
 }

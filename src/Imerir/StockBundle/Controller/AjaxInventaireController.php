f109ed79ce4bdcaec7cd26e1ad5aaa6b58912054
Knay <payen.john12@gmail.com>
2015-02-23 09:32:56 +0100

    dsf

--- src/Imerir/StockBundle/Controller/AjaxInventaireController.php
+++ src/Imerir/StockBundle/Controller/AjaxInventaireController.php
@@ -9,9 +9,25 @@ class AjaxInventaireController extends Controller
 {
     public function getAttributsAction()
     {
+    	$nom = $this->getRequest()->request->get('nom');
+    	if (null === $nom)
+    		$nom = '';
+    	
     	$soap = $this->get('noyau_soap'); // Récup module soap
-    	$attributs = $soap->call('getAttributFromNomProduit', array('nom'=>$this->getRequest()->request->get('nom')));
+    	$attributs = $soap->call('getAttributFromNomProduit', array('nom'=>$nom));
     	
         return new JsonResponse($attributs);
+    }
+    
+    public function getArticleFromCodeBarreAction()
+    {
+    	$codeBarre = $this->getRequest()->request->get('codeBarre');
+    	if (null === $codeBarre) 
+    		$codeBarre = '';
+    	
+    	$soap = $this->get('noyau_soap'); // Récup module soap
+    	$attributs = $soap->call('getArticleFromCodeBarre', array('codeBarre'=>$codeBarre));
+    	 
+    	return new JsonResponse($attributs);
     }
 }
--- src/Imerir/StockBundle/Controller/InventaireController.php
+++ src/Imerir/StockBundle/Controller/InventaireController.php
@@ -14,13 +14,47 @@ class InventaireController extends Controller
     	$return_produits = $soap->call('getProduit', array('count'=>0, 'offset'=>0, 'nom'=>'', 'ligneproduit'=>''));
     	$produitsRetour = json_decode($return_produits);
     	
-    	
-    	
     	$attributs = $soap->call('getAttributFromNomProduit', array('nom'=>$produitsRetour[0]->p));
 
         return $this->render('ImerirStockBundle::inventaire.html.twig', 
         	   array('produit'=>$produitsRetour,
         			 'attributs'=>json_decode($attributs)
                ));
+    }
+    
+    public function saveAction() {
+    	$soap = $this->get('noyau_soap');
+    	$req = $this->getRequest()->request;
+    	$tabArticle = array();
+    	
+    	//on récupère tous les produits
+    	$return_produits = $soap->call('getProduit', array('count'=>0, 'offset'=>0, 'nom'=>'', 'ligneproduit'=>''));
+    	$produitsRetour = json_decode($return_produits);
+    	
+    	$retSoapAttributs = $soap->call('getAttributFromNomProduit', array('nom'=>$produitsRetour[0]->p));
+    	
+    	// On va parser la request pour former un tableau avec les articles de l'inventaire propre
+    	foreach ($req as $key => $value) {
+    		$clef = explode('_', $key);
+    		if ($clef[0] === 'produit') { // Si c'est le nom du produit concerné
+    			$tabArticle[intval($clef[1])]['produit'] = $value;
+    		}
+    		else if ($clef[0] === 'code') { // Si c'est le code barre
+    			$tabArticle[intval($clef[1])]['codeBarre'] = $value;
+    		}
+    		else if ($clef[0] === 'quantite') { // Si c'est la quantite
+    			$tabArticle[intval($clef[1])]['quantite'] = intval($value);
+    		}
+    		else if ($clef[0] === 'caract') { // Si c'est une valeur d'attribut
+    			$attributs = explode('_', $value);
+    			$tabArticle[intval($clef[1])]['attributs'][$attributs[0]] = $attributs[1];
+    		}
+    	}
+    	$resultat = $soap->call('faireInventaire', array('articles'=> json_encode($tabArticle)));
+    	
+    	return $this->render('ImerirStockBundle::inventaire.html.twig',
+    			array('produit'=>$produitsRetour,
+    				  'attributs'=>json_decode($retSoapAttributs)
+    			));
     }
 }

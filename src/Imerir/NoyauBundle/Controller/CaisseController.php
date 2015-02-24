b9814a1c2823a56f13f476df163ff9cdac7c58f3
Knay <payen.john12@gmail.com>
2015-02-24 18:11:11 +0100

    C LA CAISSEEEEE

--- src/Imerir/NoyauBundle/Controller/CaisseController.php
+++ src/Imerir/NoyauBundle/Controller/CaisseController.php
@@ -15,12 +15,46 @@ class CaisseController extends Controller
 	 * L'action appelée lorsque l'on demande la page d'accueil qui correspond à la caisse.
 	 * @return \Symfony\Component\HttpFoundation\Response
 	 */
-    public function CaisseAction()
+    public function caisseAction()
     {
     	$soap = $this->get('noyau_soap');
     	
     	$return_menu = $soap->call('getMenu', array()); // Récup du menu/sous-menu
     	$menu_sous_menu = json_decode($return_menu);
+    	
+    	return $this->render('ImerirNoyauBundle:Default:index.html.twig', array('result_menu' => $menu_sous_menu));
+    }
+    
+    /**
+     * L'action est appelée lorsque l'on valide l'achat.
+     * @return \Symfony\Component\HttpFoundation\Response La réponse HTML.
+     */
+    public function saveCaisseAction() 
+    {
+    	$soap = $this->get('noyau_soap');
+    	
+    	$req = $this->getRequest()->request;
+    	$tabArticle = array();
+    	
+    	// On va parser la request pour former un tableau avec les articles de l'inventaire propre
+    	foreach ($req as $key => $value) {
+    		$clef = explode('_', $key);
+    		
+    		if ($clef[0] === 'code') { // Si c'est le code barre
+    			$tabArticle[intval($clef[1])]['codeBarre'] = $value;
+    		}
+    		else if ($clef[0] === 'quantite') { // Si c'est la quantite
+    			$tabArticle[intval($clef[1])]['quantite'] = intval($value);
+    		}
+    		else if ($clef[0] === 'promo') { // Si c'est la promo
+    			$tabArticle[intval($clef[1])]['promo'] = intval($value);
+    		}
+    	}
+    	
+    	$return_menu = $soap->call('getMenu', array()); // Récup du menu/sous-menu
+    	$menu_sous_menu = json_decode($return_menu);
+    	
+    	$return_menu = $soap->call('enregistrerAchat', array('articles' => json_encode($tabArticle))); // On enregistre l'achat au travers de SOAP 
     	
     	return $this->render('ImerirNoyauBundle:Default:index.html.twig', array('result_menu' => $menu_sous_menu));
     }

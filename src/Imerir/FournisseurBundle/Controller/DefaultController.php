f68aaf3c3b3f798df158586c1a3dee3b8b701781
Etienne Dijon <etienne.dijon@imerir.com>
2015-03-02 14:20:44 +0100

    to youuuuuuuuuu

--- src/Imerir/FournisseurBundle/Controller/DefaultController.php
+++ src/Imerir/FournisseurBundle/Controller/DefaultController.php
@@ -39,7 +39,7 @@ class DefaultController extends Controller
         $menu_sous_menu = json_decode($return_menu);
 
         //TODO invoquer le bon twig
-        return $this->render('ImerirFournisseurBundle::ajoutFournisseur.html.twig', array('liste_fournisseurs'=>$liste_fournisseurs,'result_menu' => $menu_sous_menu));
+        return $this->render('ImerirFournisseurBundle::ajoutFournisseur.html.twig', array('liste_fournisseurs'=>$liste_fournisseurs,'result_menu' => $menu_sous_menu,'nbAdresse'=>0));
     }
 
     public function execAjoutFournisseurAction()
@@ -62,6 +62,58 @@ class DefaultController extends Controller
             $fournisseur_telephone_portable===null;
 
         $soap->call('ajoutFournisseur',array('nom' => $fournisseur_nom, 'email'=>$fournisseur_email, 'telephone_portable'=>$fournisseur_telephone_portable));
+        ////////////////////////////////////////////////////////////////////////////
+        /**
+         * PARTIE ADRESSES
+         */
+        $adresse_pays = array();
+        $adresse_ville = array();
+        $adresse_code_postal = array();
+        $adresse_num_voie = array();
+        $adresse_voie = array();
+        $adresse_num_appartement = array();
+        $adresse_telephone_fixe = array();
+
+
+        foreach ($query->request as $key => $value) {
+            if (substr($key, 0, strlen('pays')) === 'pays') {
+                array_push($adresse_pays, $value);
+            }
+            if (substr($key, 0, strlen('ville')) === 'ville') {
+                array_push($adresse_ville, $value);
+            }
+            if (substr($key, 0, strlen('code_postal')) === 'code_postal') {
+                array_push($adresse_code_postal, $value);
+            }
+            if (substr($key, 0, strlen('num_voie')) === 'num_voie') {
+                array_push($adresse_num_voie, $value);
+            }
+            if (substr($key, 0, strlen('voie')) === 'voie') {
+                array_push($adresse_voie, $value);
+            }
+            if (substr($key, 0, strlen('num_appartement')) === 'num_appartement') {
+                array_push($adresse_num_appartement, $value);
+            }
+            if (substr($key, 0, strlen('telephone_fixe')) === 'telephone_fixe') {
+                array_push($adresse_telephone_fixe, $value);
+            }
+        }
+        //PARTIE OU ON RECUPERE LA REF DU FOURNISSEUR
+        $retour_ref_fournisseur = $soap->call('getFournisseurs',array('count' => 0,'offset' => 0, 'nom' => $fournisseur_nom,
+            'email'=>$fournisseur_email, 'telephone_portable'=>$fournisseur_telephone_portable));
+
+        $ref_fournisseur = json_decode($retour_ref_fournisseur,true);
+        $ref_fournisseur_id = $ref_fournisseur[0]["id"];
+        //print_r($ref_fournisseur);
+
+        //$est_fournisseur,$ref_id,$pays,$ville, $voie, $num_voie, $code_postal, $num_appartement,$telephone_fixe
+        $soap->call('ajoutAdresse',array('est_fournisseur'=>true,'ref_id'=>$ref_fournisseur_id,'pays'=>json_encode($adresse_pays),
+            'ville'=>json_encode($adresse_ville),'voie'=>json_encode($adresse_voie),'num_voie'=>json_encode($adresse_num_voie),
+            'code_postal'=>json_encode($adresse_code_postal),'num_appartement'=>json_encode($adresse_num_appartement),
+            'telephone_fixe'=>json_encode($adresse_telephone_fixe)));
+
+        ////////////////////////////////////////////////////////////////////////////
+
 
         //TODO ajouter l'action getFournisseurs qui renvoie le nom, le mail et le num de tel
         $return = $soap->call('getFournisseurs',array('count' => 0,'offset' => 0, 'nom' => '', 'email'=>'', 'telephone_portable'=>''));

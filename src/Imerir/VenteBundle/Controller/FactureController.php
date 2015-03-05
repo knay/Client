0442319bb52c3b5cab359778a83dac205653a88f
david ansillon <david.ansillon@gmail.com>
2015-03-05 09:11:08 +0100

    tete

--- src/Imerir/VenteBundle/Controller/FactureController.php
+++ src/Imerir/VenteBundle/Controller/FactureController.php
@@ -19,17 +19,36 @@ class FactureController extends Controller
     	catch(\SoapFault $e) {
     		$erreur.=$e->getMessage();
     	}
-    	/*
+    	
     	try {
-    		$return_moyenne = $soap->call('statsVenteMoyenneParMois', array('nbMois' => 1)); // On récupère le menu/sous-menu
-    		$stats = json_decode($return_moyenne);
+    		$return_facture = $soap->call('getAllFacture', array()); // On récupère le menu/sous-menu
+    		$facture = json_decode($return_facture);
     	}
     	catch(\SoapFault $e) {
     		$erreur.=$e->getMessage();
     	}
     	
-    	*/
+    	
     	 
-    	return $this->render('ImerirVenteBundle::facture.html.twig', array('result_menu' => $menu_sous_menu));
+    	return $this->render('ImerirVenteBundle::facture.html.twig', array('result_menu' => $menu_sous_menu,'result_facture'=>$facture));
+    }
+    
+    public function factureDetailFromLigneFacture(){
+    	$soap = $this->get('noyau_soap');
+        $query = $this->get('request');
+        
+        //on recuperer les variables en post ici variable 
+        $id_tableau = $query->request->get('id_select');
+        $ref_tableau = $query->request->get('ref_select');
+
+        $result = array("id"=>$id_tableau,"ref"=>$ref_tableau);
+        
+        //on recupere le menu et sous menu
+        $return_menu = $soap->call('getMenu', array());
+        $menu_sous_menu = json_decode($return_menu);
+        
+        return $this->render('ImerirVenteBundle::facture.html.twig',
+            array('result_menu' => $menu_sous_menu,'resultat'=>$result));
+   
     }
 }

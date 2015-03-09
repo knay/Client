bb4dd27301bac4d8b88f9e2b15e6787c4b1019f8
Etienne Dijon <etienne.dijon@imerir.com>
2015-03-09 10:31:30 +0100

    suce

--- src/Imerir/ContactBundle/Controller/StatsController.php
+++ src/Imerir/ContactBundle/Controller/StatsController.php
@@ -62,4 +62,32 @@ class StatsController extends Controller
 																    			 'erreur' => $erreur));
     }
 
+	public function contactVillesAction()
+	{
+		$soap = $this->get('noyau_soap'); // Récup du client SOAP depuis le service.
+		$menu_sous_menu = array();
+		$erreur = '';
+		$stats = array();
+
+		try {
+			$return_menu = $soap->call('getMenu', array()); // On récupère le menu/sous-menu
+			$menu_sous_menu = json_decode($return_menu);
+		}
+		catch(\SoapFault $e) {
+			$erreur.=$e->getMessage();
+		}
+
+		try {
+			$return = $soap->call('getNombreContactsParVille', array()); // On récupère le menu/sous-menu
+			$stats = json_decode($return);
+		}
+		catch(\SoapFault $e) {
+			$erreur.=$e->getMessage();
+		}
+
+		return $this->render('ImerirContactBundle::statsClientsVille.html.twig', array('result_menu' => $menu_sous_menu,
+			'stats' => $stats,
+			'erreur' => $erreur));
+	}
+
 }

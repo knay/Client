ce27bad9fde05863128473dff1ea016e450972f2
Knay <payen.john12@gmail.com>
2015-03-11 18:01:49 +0100

    SUCEEEEE

--- src/Imerir/VenteBundle/Controller/RetourClientController.php
+++ src/Imerir/VenteBundle/Controller/RetourClientController.php
@@ -47,4 +47,45 @@ class RetourClientController extends Controller
     																			'result_facture'=>$facture,
     																	        'erreur'=>$erreur));
     }
+    
+    public function faireRetourClientDetailsAction() {
+    	$soap = $this->get('noyau_soap');
+    	$query = $this->get('request')->request;
+    	$erreur='';
+    	$detail_facture = array();
+    	$facture = array();
+    	$numero = $query->get('id_facture');
+    	
+    	if($numero === null)
+    		$numero = 0;
+    	
+    	try {
+    		$return_menu = $soap->call('getMenu', array()); // On récupère le menu/sous-menu
+    		$menu_sous_menu = json_decode($return_menu);
+    	}
+    	catch(\SoapFault $e) {
+    		$erreur.=$e->getMessage();
+    	}
+    	 
+    	try {
+    		$return_facture_detail = $soap->call('getDetailFromOneFacture', array('numero'=>$numero)); // On récupère detail de la facture
+    		$detail_facture = json_decode($return_facture_detail);
+    	}
+    	catch(\SoapFault $e) {
+    		$erreur.=$e->getMessage();
+    	}
+    	 
+    	try {
+    		$return_facture = $soap->call('getAllFacture', array()); // On récupère le menu/sous-menu
+    		$facture = json_decode($return_facture);
+    	}
+    	catch(\SoapFault $e) {
+    		$erreur.=$e->getMessage();
+    	}
+    	 
+    	return $this->render('ImerirVenteBundle::retourClient.html.twig', array('result_menu' => $menu_sous_menu,
+    																			'result_facture' => $facture,
+    																			'detail_facture'=>$detail_facture, 
+    																			'erreur' => $erreur));
+    }
 }

9cc29ed8754df742d11518689d28435434192765
david ansillon <david.ansillon@gmail.com>
2015-03-13 17:45:16 +0100

    dd

--- src/Imerir/VenteBundle/Controller/StatsController.php
+++ src/Imerir/VenteBundle/Controller/StatsController.php
@@ -9,9 +9,18 @@ class StatsController extends Controller
     public function indexAction()
     {
     	$soap = $this->get('noyau_soap'); // Récup du client SOAP depuis le service.
+    	$query = $this->get('request');
+    	$valeur_comparaison = $query->request->get('comparaison');
     	$menu_sous_menu = array();
+    	$stats = array();
+    	$mois='';
+    	$annee='';
     	$erreur = '';
-    	 
+    	$date = $query->request->get('date');
+    	$axeX_n_moins_un = array();
+    	$axeY_n = array();
+    	$axeY_n_moins_un = array();
+    	
     	try {
     		$return_menu = $soap->call('getMenu', array()); // On récupère le menu/sous-menu
     		$menu_sous_menu = json_decode($return_menu);
@@ -19,26 +28,59 @@ class StatsController extends Controller
     	catch(\SoapFault $e) {
     		$erreur.=$e->getMessage();
     	}
-    	
-    	try {
-    		$return_moyenne = $soap->call('statsVenteMoyenneParMois', array('nbMois' => 1)); // On récupère le menu/sous-menu
-    		$stats = json_decode($return_moyenne);
-    	}
-    	catch(\SoapFault $e) {
-    		$erreur.=$e->getMessage();
+    	//Si une date est selectionner on recupere le mois et l'ann�e
+    	if($date != ''){
+    		$mois = $this->before('-',$date);
+    		$annee = $this->after('-',$date);
     	}
     	
-    	$axeX = array();
-    	$axeY = array();
-    	foreach ($stats as $valeur) {
-    		array_push($axeX, $valeur->jour);
-    		array_push($axeY, $valeur->montant);
+    	// L'utilisateur fait une recherche par date
+    	if($date != ''){
+    		try {
+    			$return_moyenne_defaut = $soap->call('statsVenteMoyenneParMoisParDefaut', array($mois,$annee)); 
+    			$stats = json_decode($return_moyenne_defaut);
+    		}
+    		catch(\SoapFault $e) {
+    			$erreur.=$e->getMessage();
+    		}
     	}
-    	 
+    	// L'utilisateur ne fait pas de recherche sur la date donc par defaut on prend le mois courant
+    	else{
+    		try {
+    			$return_moyenne_defaut = $soap->call('statsVenteMoyenneParMoisParDefaut', array($mois,$annee)); 
+    			$stats = json_decode($return_moyenne_defaut);
+    		}
+    		catch(\SoapFault $e) {
+    			$erreur.=$e->getMessage();
+    		}
+    		
+    		foreach ($stats[0] as $valeur){
+    			array_push($axeY_n, $valeur->montant_n);
+    		}
+    		
+    		foreach ($stats[1] as $valeur){
+    			array_push($axeX_n_moins_un, $valeur->jour_n_moins_un);
+    			array_push($axeY_n_moins_un, $valeur->montant_n_moins_un);
+    		}
+    	}
+
     	return $this->render('ImerirVenteBundle::statsVenteMois.html.twig', array('result_menu' => $menu_sous_menu,
-    			                                                         'axeX' => $axeX,
-    			                                                         'axeY' => $axeY,
+    			                                                         'axeX_n_moins_un' => $axeX_n_moins_un,
+    			                                                         'axeY_n' => $axeY_n,
+															    		 'axeY_n_moins_un' => $axeY_n_moins_un,
     		                	                                         'erreur' => $erreur));
+    }
+    
+    public function after($caractere, $date_entiere)
+    {
+    	if (!is_bool(strpos($date_entiere, $caractere)))
+    		return substr($date_entiere, strpos($date_entiere,$caractere)+strlen($caractere));
+    }
+    
+    
+    public function before($caractere, $date_entiere)
+    {
+        return substr($date_entiere, 0, strpos($date_entiere, $caractere));
     }
     
     public function topVenteAction()
